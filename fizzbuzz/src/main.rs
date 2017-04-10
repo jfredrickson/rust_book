5b48ce89199cfc33fbffaa1472801829d24c3fae
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2017-04-10 17:55:39 -0400

    don't need to use blocks

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     for n in 1..101 {
         let output = match (n % 3, n % 5) {
-            (0, 0) => { "FizzBuzz".to_string() },
-            (0, _) => { "Fizz".to_string() },
-            (_, 0) => { "Buzz".to_string() },
-            _ => { n.to_string() }
+            (0, 0) => "FizzBuzz".to_string(),
+            (0, _) => "Fizz".to_string(),
+            (_, 0) => "Buzz".to_string(),
+            _ => n.to_string()
         };
         println!("{}", output);
     }
